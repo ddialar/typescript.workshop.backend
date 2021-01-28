0866d17f195dab9ada55200b0bf8aca41d2a17ba
ddialar <ddialar@gmail.com>
2021-01-28 05:31:46 +0000

    chore(domain.models): Define the 'UpdateUserPayloadDomainModel' and use it in the specific areas of data sources and DTOs.

--- src/infrastructure/dataSources/user.datasource.ts
+++ src/infrastructure/dataSources/user.datasource.ts
@@ -1,5 +1,4 @@
-import { UserDomainModel, NewUserDomainModel, UserProfileDomainModel } from '@domainModels'
-import { NewUserProfileDto, UpdateUserPayloadDto } from '@infrastructure/dtos'
+import { UserDomainModel, NewUserDomainModel, UserProfileDomainModel, UpdateUserPayloadDomainModel, NewUserProfileDomainModel } from '@domainModels'
 import { mapUserFromDtoToDomainModel, mapUserFromDtoToProfileDomainModel } from '@infrastructure/mappers'
 import { mongodb } from '@infrastructure/orm'
 
@@ -9,10 +8,11 @@ export const createUser = async (newUserData: NewUserDomainModel): Promise<void>
 
 export const getUserByUsername = async (username: string): Promise<UserDomainModel | null> =>
   mapUserFromDtoToDomainModel(await mongodb.requests.user.getByUsername(username))
+
 export const getUserProfileById = async (userId: string): Promise<UserProfileDomainModel | null> =>
   mongodb.requests.user.getProfileById(userId)
 
-export const updateUserById = async (userId: string, updatedUserData: UpdateUserPayloadDto): Promise<UserDomainModel | null> =>
+export const updateUserById = async (userId: string, updatedUserData: UpdateUserPayloadDomainModel): Promise<UserDomainModel | null> =>
   mapUserFromDtoToDomainModel(await mongodb.requests.user.updateById(userId, updatedUserData))
 export const updateUserProfileById = async (userId: string, newUserProfileData: NewUserProfileDto): Promise<UserProfileDomainModel | null> =>
   mapUserFromDtoToProfileDomainModel(await mongodb.requests.user.updateById(userId, newUserProfileData))
--- src/infrastructure/dtos/user.dtos.ts
+++ src/infrastructure/dtos/user.dtos.ts
@@ -1,8 +1,8 @@
-import { UserDomainModel, NewUserDomainModel, UserProfileDomainModel, NewUserProfileDomainModel } from '@domainModels'
+import { UserDomainModel, NewUserDomainModel, UserProfileDomainModel, NewUserProfileDomainModel, UpdateUserPayloadDomainModel } from '@domainModels'
 
 export type UserDto = Omit<UserDomainModel, 'id'> & { _id: string }
 export type UserProfileDto = UserProfileDomainModel
 export type NewUserProfileDto = NewUserProfileDomainModel
 export type NewUserInputDto = Omit<NewUserDomainModel, 'username' >
 export type NewUserDatabaseDto = NewUserDomainModel
-export type UpdateUserPayloadDto = Omit<Partial<UserDomainModel>, 'id' | 'username' | 'email' | 'createdAt' | 'updatedAt'>
+export type UpdateUserPayloadDto = UpdateUserPayloadDomainModel
