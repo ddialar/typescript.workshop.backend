60d59a2e7fd2ab1bb3dc9959f857be96671d4fcf
ddialar <ddialar@gmail.com>
2021-01-29 06:52:17 +0000

    docs(user.fixtures): Type some notes in order to run a refactor.

--- src/test/fixtures/user.fixtures.ts
+++ src/test/fixtures/user.fixtures.ts
@@ -6,5 +6,25 @@ import { avatarUrls } from './assets/avatarUrls.json'
 export const testingNonValidUserId = random.uuid().split('-').join('').slice(0, 24)
 export const testingNonPersistedUsername = 'non.persisted@mail.com'
 
+// TODO Use type annotation before assigninig the JSON values in order to ensure a corect typing.
+/**
+export interface UserDomainModel {
+  id: string
+  username: string
+  password: string
+  email: string
+  name: string | null
+  surname: string | null
+  avatar: string | null
+  token: string | null
+  enabled: boolean
+  deleted: boolean
+  lastLoginAt: string | null
+  createdAt: string
+  updatedAt: string
+}
+type FixturedUser = Omit<UserDomainModel, 'createdAt' | 'updatedAt'>
+export const testingUsers: FixturedUser = mockedUsers
+*/
 export const testingUsers = mockedUsers
 export const testingAvatarUrls = avatarUrls
