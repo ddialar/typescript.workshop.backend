e6e4696ea69cce24b80b9c1ec8ccbbbaa4b2f768
ddialar <ddialar@gmail.com>
2021-01-31 07:50:20 +0000

    refactor(user.domain.models): Edit the 'NewUserProfileDomainModel' type in order to reference the main 'UserDomainModel'.

--- src/domain/models/user.domain.models.ts
+++ src/domain/models/user.domain.models.ts
@@ -17,5 +17,5 @@ export interface UserDomainModel {
 export type NewUserDomainModel = Pick<UserDomainModel, 'username' | 'password' | 'email' | 'name' | 'surname' | 'avatar'>
 export type UpdateUserPayloadDomainModel = Omit<Partial<UserDomainModel>, 'id' | 'username' | 'email' | 'createdAt' | 'updatedAt'>
 export type UserProfileDomainModel = Pick<UserDomainModel, 'username' | 'email' | 'name' | 'surname' | 'avatar'>
-export type NewUserProfileDomainModel = Partial<Pick<UserProfileDomainModel, 'name' | 'surname' | 'avatar'>>
+export type NewUserProfileDomainModel = Partial<Pick<UserDomainModel, 'name' | 'surname' | 'avatar'>>
 export type AuthenticatedUserDomainModel = Pick<UserDomainModel, 'token'>
