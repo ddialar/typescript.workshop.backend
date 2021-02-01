54772d8a22e143dc228d766c8921dc84d6059975
ddialar <ddialar@gmail.com>
2021-02-01 17:13:19 +0000

    chore(fixtures): Include new static values in the user fixtures.

--- src/test/fixtures/user.fixtures.ts
+++ src/test/fixtures/user.fixtures.ts
@@ -4,7 +4,12 @@ import mockedUsers from './assets/users.json'
 import { avatarUrls } from './assets/avatarUrls.json'
 import { UserDomainModel } from '@domainModels'
 
-export const testingNonValidUserId = random.uuid().split('-').join('').slice(0, 24)
+const generateRandomId = () => random.uuid().split('-').join('').slice(0, 24)
+
+export const testingNonValidPostId = generateRandomId()
+export const testingNonValidLikeOwnerId = generateRandomId()
+export const testingNonValidUserId = generateRandomId()
+
 export const testingNonPersistedUsername = 'non.persisted@mail.com'
 
 export const testingUsers: UserDomainModel[] = mockedUsers
