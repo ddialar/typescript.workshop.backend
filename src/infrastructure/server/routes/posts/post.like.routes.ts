44236de791aa182048281b919dcc85d2b38a26c6
ddialar <ddialar@gmail.com>
2021-02-01 17:07:34 +0000

    refactor(post.like.routes): Remove non necessary as assertion.

--- src/infrastructure/server/routes/posts/post.like.routes.ts
+++ src/infrastructure/server/routes/posts/post.like.routes.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { likePost, dislikePost } from '@domainServices'
 import { ensureAuthenticated } from '../../middlewares'
-import { UserDomainModel } from '@domainModels'
 import { RequestDto } from '@infrastructure/server/serverDtos'
 
 import { createLogger } from '@common'
@@ -10,7 +9,7 @@ const logger = createLogger('post.endpoints')
 const postLikeRoutes = express.Router()
 
 postLikeRoutes.post('/like', ensureAuthenticated, async (req: RequestDto, res, next) => {
-  const { id, name, surname, avatar } = req.user as UserDomainModel
+  const { id, name, surname, avatar } = req.user!
   const { postId } = req.body
 
   logger.debug(`Liking post '${postId}' by user '${id}'.`)
@@ -23,13 +22,13 @@ postLikeRoutes.post('/like', ensureAuthenticated, async (req: RequestDto, res, n
 })
 
 postLikeRoutes.delete('/like', ensureAuthenticated, async (req: RequestDto, res, next) => {
-  const { id: likeOwnerId } = req.user as UserDomainModel
+  const { id: likeOwnerId } = req.user!
   const { postId } = req.body
 
   logger.debug(`Disliking post '${postId}' by user '${likeOwnerId}'.`)
 
   try {
-    await dislikePost(postId as string, likeOwnerId as string)
+    await dislikePost(postId as string, likeOwnerId)
     res.send()
   } catch (error) {
     next(error)
