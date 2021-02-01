998aaf1f2844b227e66d79e04254220a63647483
ddialar <ddialar@gmail.com>
2021-02-01 17:12:27 +0000

    chore(deletePostLike.route.test): Clean up the testing suite in order to make it more readable.

--- src/infrastructure/server/routes/posts/test/deletePostLike.route.test.ts
+++ src/infrastructure/server/routes/posts/test/deletePostLike.route.test.ts
@@ -4,18 +4,18 @@ import { server } from '@infrastructure/server'
 import { mongodb } from '@infrastructure/orm'
 
 import { BAD_REQUEST, OK, UNAUTHORIZED, INTERNAL_SERVER_ERROR, NOT_FOUND } from '@errors'
-import { PostDomainModel, UserDomainModel } from '@domainModels'
 import { postDataSource } from '@infrastructure/dataSources'
 import { PostDto } from '@infrastructure/dtos'
 
 import {
   testingLikedAndCommentedPersistedDtoPosts,
-  testingLikedAndCommentedPersistedDomainModelPosts,
   testingUsers,
+  testingDtoFreeUsers,
   testingValidJwtTokenForNonPersistedUser,
   testingExpiredJwtToken,
   cleanUsersCollectionFixture,
-  cleanPostsCollectionFixture
+  cleanPostsCollectionFixture,
+  testingNonValidPostId
 } from '@testingFixtures'
 
 const POSTS_LIKE_PATH = '/posts/like'
@@ -24,42 +24,37 @@ describe('[API] - Posts endpoints', () => {
   describe(`[DELETE] ${POSTS_LIKE_PATH}`, () => {
     const { connect, disconnect, models: { User, Post } } = mongodb
 
-    const mockedDtoPosts = testingLikedAndCommentedPersistedDtoPosts as PostDto[]
-    const mockedCompleteDtoPost = JSON.parse(JSON.stringify(mockedDtoPosts[0]))
-    const mockedEmptyLikesDtoPost = JSON.parse(JSON.stringify(mockedDtoPosts[1]))
-    mockedEmptyLikesDtoPost.likes = []
+    const [selectedPost, emptyLikesPost] = testingLikedAndCommentedPersistedDtoPosts
+    emptyLikesPost.likes = []
 
-    const resultPosts = testingLikedAndCommentedPersistedDomainModelPosts as PostDomainModel[]
-    const selectedPost = resultPosts[0]
-    const selectedLike = selectedPost.likes[0]
-    const selectedLikeOwnerId = selectedLike.id
-    const mockedNonValidPostId = resultPosts[1].owner.id as string
-    const mockedNoLikerUserId = resultPosts[1].owner.id as string
+    const nonValidPostId = testingNonValidPostId
 
+    const [{ userId: selectedLikeOwnerId }] = selectedPost.likes
     const {
-      username: ownerUsername,
-      password: ownerPassword,
-      email: ownerEmail,
-      token: ownerValidToken
-    } = testingUsers.find(({ id }) => id === selectedLikeOwnerId) as UserDomainModel
+      username: likeOwnerUsername,
+      password: likeOwnerPassword,
+      email: likeOwnerEmail,
+      token: likeOwnerValidToken
+    } = testingUsers.find(({ id }) => id === selectedLikeOwnerId)!
     const mockedPostLikeOwner = {
       _id: selectedLikeOwnerId,
-      username: ownerUsername,
-      password: ownerPassword,
-      email: ownerEmail
+      username: likeOwnerUsername,
+      password: likeOwnerPassword,
+      email: likeOwnerEmail
     }
 
+    const [{ userId: freeUserId }] = testingDtoFreeUsers
     const {
-      username: noLikerUsername,
-      password: noLikerUserPassword,
-      email: noLikerUserEmail,
-      token: noLikerUserValidToken
-    } = testingUsers.find(({ id }) => id === mockedNoLikerUserId) as UserDomainModel
+      username: freeUserUsername,
+      password: freeUserPassword,
+      email: freeUserEmail,
+      token: noLikeOwnerToken
+    } = testingUsers.find(({ id }) => id === freeUserId)!
     const mockedUnauthorizedUserToBePersisted = {
-      _id: mockedNoLikerUserId,
-      username: noLikerUsername,
-      password: noLikerUserPassword,
-      email: noLikerUserEmail
+      _id: freeUserId,
+      username: freeUserUsername,
+      password: freeUserPassword,
+      email: freeUserEmail
     }
 
     let request: SuperTest<Test>
@@ -73,7 +68,7 @@ describe('[API] - Posts endpoints', () => {
 
     beforeEach(async () => {
       await cleanPostsCollectionFixture()
-      await Post.insertMany([mockedCompleteDtoPost, mockedEmptyLikesDtoPost])
+      await Post.insertMany([selectedPost, emptyLikesPost])
     })
 
     afterAll(async () => {
@@ -82,9 +77,9 @@ describe('[API] - Posts endpoints', () => {
       await disconnect()
     })
 
-    it('must return OK (200) and delete the provided comment', async (done) => {
-      const token = `bearer ${ownerValidToken}`
-      const postId = selectedPost.id as string
+    it('must return OK (200) and delete the provided like', async (done) => {
+      const token = `bearer ${likeOwnerValidToken}`
+      const { _id: postId } = selectedPost
 
       await request
         .delete(POSTS_LIKE_PATH)
@@ -95,15 +90,15 @@ describe('[API] - Posts endpoints', () => {
           const { likes: updatedDtoLikes } = (await Post.findById(postId))?.toJSON() as PostDto
 
           expect(updatedDtoLikes).toHaveLength(selectedPost.likes.length - 1)
-          expect(updatedDtoLikes.map(({ userId }) => userId).includes(selectedLikeOwnerId)).toBeFalsy()
+          expect(updatedDtoLikes.map(({ userId }) => userId).includes(selectedLikeOwnerId!)).toBeFalsy()
         })
 
       done()
     })
 
     it('must return OK (200) but not modify the selected post nor throw any error when the provided user has not liked the post', async (done) => {
-      const token = `bearer ${noLikerUserValidToken}`
-      const postId = selectedPost.id as string
+      const token = `bearer ${noLikeOwnerToken}`
+      const { _id: postId } = selectedPost!
 
       await request
         .delete(POSTS_LIKE_PATH)
@@ -121,7 +116,7 @@ describe('[API] - Posts endpoints', () => {
 
     it('must return UNAUTHORIZED (401) error when we send an expired token', async (done) => {
       const token = `bearer ${testingExpiredJwtToken}`
-      const postId = selectedPost.id as string
+      const { _id: postId } = selectedPost!
       const expectedErrorMessage = 'Token expired'
 
       await request
@@ -138,7 +133,7 @@ describe('[API] - Posts endpoints', () => {
 
     it('must return BAD_REQUEST (400) error when we send a token of non recorded user', async (done) => {
       const token = `bearer ${testingValidJwtTokenForNonPersistedUser}`
-      const postId = selectedPost.id as string
+      const { _id: postId } = selectedPost!
       const expectedErrorMessage = 'User does not exist'
 
       await request
@@ -154,8 +149,8 @@ describe('[API] - Posts endpoints', () => {
     })
 
     it('must return NOT_FOUND (404) when the provided post ID doesn\'t exist', async (done) => {
-      const token = `bearer ${ownerValidToken}`
-      const postId = mockedNonValidPostId
+      const token = `bearer ${likeOwnerValidToken}`
+      const postId = nonValidPostId
       const expectedErrorMessage = 'Post not found'
 
       await request
@@ -175,8 +170,8 @@ describe('[API] - Posts endpoints', () => {
         throw new Error('Testing error')
       })
 
-      const token = `bearer ${ownerValidToken}`
-      const postId = selectedPost.id as string
+      const token = `bearer ${likeOwnerValidToken}`
+      const { _id: postId } = selectedPost!
       const expectedErrorMessage = 'Internal Server Error'
 
       await request
@@ -198,8 +193,8 @@ describe('[API] - Posts endpoints', () => {
         throw new Error('Testing error')
       })
 
-      const token = `bearer ${ownerValidToken}`
-      const postId = selectedPost.id as string
+      const token = `bearer ${likeOwnerValidToken}`
+      const { _id: postId } = selectedPost!
       const expectedErrorMessage = 'Internal Server Error'
 
       await request
