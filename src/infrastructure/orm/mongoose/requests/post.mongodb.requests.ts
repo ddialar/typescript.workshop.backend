51cbd3bd36e773f0819598fb208e48f6c71714d7
ddialar <ddialar@gmail.com>
2021-02-01 16:55:35 +0000

    feat(post.mongodb.requests): Implement aggregation framework in the 'getLikeByOwnerId' method.

--- src/infrastructure/orm/mongoose/requests/post.mongodb.requests.ts
+++ src/infrastructure/orm/mongoose/requests/post.mongodb.requests.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose'
 import { Post } from '../models'
 import { PostCommentDto, PostDto, PostLikeDto } from '@infrastructure/dtos'
 
@@ -20,10 +21,7 @@ export const getAll = async (): Promise<PostDto[] | null> => {
   return retrievedPosts ? JSON.parse(JSON.stringify(retrievedPosts)) as PostDto[] : null
 }
 
-export const getById = async (postId: string): Promise<PostDto | null> => {
-  const retrievedPost = await Post.findById(postId).lean()
-  return retrievedPost ? JSON.parse(JSON.stringify(retrievedPost)) as PostDto : null
-}
+export const getById = async (postId: string): Promise<PostDto | null> => Post.findById(postId).lean<PostDto>()
 
 export const deletePost = async (postId: string): Promise<void> => {
   const conditions = { _id: postId }
@@ -36,16 +34,48 @@ export const getComment = async (postId: string, commentId: string): Promise<Pos
   return (JSON.parse(JSON.stringify(retrievedPost)) as PostDto).comments.find(({ _id }) => _id === commentId) || null
 }
 
-export const getLikeByOwnerId = async (postId: string, ownerId: string): Promise<PostLikeDto | null> => {
-  // REFACTOR Research about how to retrieve the selected comment from the post, using aggregation framework.
-  const retrievedPost = await Post.findById({ _id: postId }).lean()
-  return (JSON.parse(JSON.stringify(retrievedPost)) as PostDto).likes.find(({ userId }) => userId === ownerId) || null
-}
-
 export const deleteComment = async (postId: string, commentId: string): Promise<void> => {
   const conditions = { _id: postId }
   const update = { $pull: { comments: { _id: commentId } } }
   await Post.findOneAndUpdate(conditions, update)
+}
+
+export const getLikeByOwnerId = async (postId: string, ownerId: string): Promise<PostLikeDto | null> => {
+  const postMatcher = { $match: { _id: Types.ObjectId(postId) } }
+  const likeMatcher = {
+    $project: {
+      _id: 0,
+      likes: {
+        $arrayElemAt: [{
+          $filter: {
+            input: '$likes',
+            as: 'item',
+            cond: { $eq: ['$$item.userId', `${ownerId}`] }
+          }
+        }, 0]
+      }
+    }
+  }
+  const likeReduction = {
+    $project: {
+      _id: '$likes._id',
+      userId: '$likes.userId',
+      name: '$likes.name',
+      surname: '$likes.surname',
+      avatar: '$likes.avatar',
+      createdAt: '$likes.createdAt',
+      updatedAt: '$likes.updatedAt'
+    }
+  }
+
+  const [retrievedPostLike] = await Post.aggregate<PostLikeDto>([
+    postMatcher,
+    likeMatcher,
+    likeReduction
+  ])
+
+  // NOTE If no coincidences are found, the aggregation returns and empty object.
+  return Object.keys(retrievedPostLike).length ? retrievedPostLike : null
 }
 
 export const like = async (postId: string, owner: PostLikeDto): Promise<PostDto | null> => {
