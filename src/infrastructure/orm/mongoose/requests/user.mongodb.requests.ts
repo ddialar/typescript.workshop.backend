c83607467b910d5712dca2634ddf2c9d5fb49477
ddialar <ddialar@gmail.com>
2021-02-01 22:36:56 +0000

    refactor(user.mongodb.requests): Use generics in 'lean' method.

--- src/infrastructure/orm/mongoose/requests/user.mongodb.requests.ts
+++ src/infrastructure/orm/mongoose/requests/user.mongodb.requests.ts
@@ -6,21 +6,16 @@ export const create = async (newUserData: NewUserDatabaseDto): Promise<void> => 
   await (new User(newUserData)).save()
 }
 
-export const getByUsername = async (username: string): Promise<UserDto | null> => {
-  const user = await User.findOne({ username }).lean()
-  return user ? JSON.parse(JSON.stringify(user)) : user
-}
+export const getByUsername = async (username: string): Promise<UserDto | null> =>
+  User.findOne({ username }).lean<UserDto>()
 
-export const getProfileById = async (id: string): Promise<UserProfileDto | null> => {
-  const profile = await User.findById(id).select('-_id username email name surname avatar').lean()
-  return profile ? JSON.parse(JSON.stringify(profile)) : profile
-}
+export const getProfileById = async (id: string): Promise<UserProfileDto | null> =>
+  User.findById(id).select('-_id username email name surname avatar').lean<UserProfileDto>()
 
 export const updateById = async (id: string, payload: UpdateUserPayloadDto): Promise<UserDto | null> => {
   // NOTE: Besides to define the fields as 'immutable' in the schema definition, it's required to use the 'strict' option 'cos in opposite, the field can be overwriten anyway :(
   const update = payload
   const options: QueryFindOneAndUpdateOptions = { new: true, strict: 'throw' }
 
-  const updatedUser = await User.findByIdAndUpdate(id, update, options)
-  return updatedUser ? updatedUser.toJSON() as UserDto : null
+  return await User.findByIdAndUpdate(id, update, options)
 }
