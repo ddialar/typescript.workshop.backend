b7dfafe3de64ab3f443015a25c29c12777f3e17c
ddialar <ddialar@gmail.com>
2021-02-02 20:10:09 +0000

    test(checkToken.test): Implement the testing suite. All tests are in green.

---
+++ src/domain/services/test/authentication.services/checkToken.test.ts
@@ -0,0 +1,38 @@
+import { TokenExpiredError, CheckingTokenError } from '@errors'
+import { testingExpiredJwtToken, testingMalformedJwtToken, testingUsers } from '@testingFixtures'
+
+import { checkToken } from '@domainServices'
+
+const [{ id: userId, token: validToken, username }] = testingUsers
+
+describe('[SERVICES] Authentication - checkToken', () => {
+  it('must return the token content decoded', () => {
+    const token = validToken
+    const result = checkToken(token)
+    const expectedFields = ['exp', 'iat', 'sub', 'username']
+    const retrievedTokenFields = Object.keys(result).sort()
+    expect(retrievedTokenFields.sort()).toEqual(expectedFields.sort())
+
+    expect(result.exp).toBeGreaterThan(0)
+    expect(result.iat).toBeGreaterThan(0)
+    expect(result.sub).toBe(userId)
+    expect(result.username).toBe(username)
+  })
+
+  it('must throw an UNAUTHORIZED (401) error when the token is expired', () => {
+    const token = testingExpiredJwtToken
+    const expectedError = new TokenExpiredError(`Token '${token}' expired`)
+
+    expect(() => checkToken(token)).toThrowError(expectedError)
+  })
+
+  it('must throw an INTERNAL_SERVER_ERROR (500) when the verification process fails', async (done) => {
+    const token = testingMalformedJwtToken
+    const errorMessage = 'invalid token'
+    const expectedError = new CheckingTokenError(`Error ckecking token '${token}'. ${errorMessage}`)
+
+    expect(() => checkToken(token)).toThrowError(expectedError)
+
+    done()
+  })
+})
--- src/test/fixtures/authentication.fixtures.ts
+++ src/test/fixtures/authentication.fixtures.ts
@@ -7,6 +7,7 @@ import {
 } from './assets/authentication.json'
 
 export const testingValidJwtTokenForNonPersistedUser: string = validJwtTokenForNonPersistedUser.value
+export const testingMalformedJwtToken: string = validJwtTokenForNonPersistedUser.value.substring(1)
 export const testingExpiredJwtToken: string = expiredJwtToken.value
 export const testingValidHashedPassword: string = validHashedPassword.value
 export const testingValidPlainPassword: string = validPlainPassword.value
