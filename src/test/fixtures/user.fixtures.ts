b275eaf04478d471b675e37dc5dbe78c0285db50
ddialar <ddialar@gmail.com>
2021-02-02 17:33:36 +0000

    chore(user.fixtures): Include new fixed values.

--- src/test/fixtures/user.fixtures.ts
+++ src/test/fixtures/user.fixtures.ts
@@ -1,14 +1,9 @@
-
-import { random } from 'faker'
 import mockedUsers from './assets/users.json'
 import { avatarUrls } from './assets/avatarUrls.json'
 import { UserDomainModel } from '@domainModels'
+import { generateMockedMongoDbId } from './utils.fixtures'
 
-const generateRandomId = () => random.uuid().split('-').join('').slice(0, 24)
-
-export const testingNonValidPostId = generateRandomId()
-export const testingNonValidLikeOwnerId = generateRandomId()
-export const testingNonValidUserId = generateRandomId()
+export const testingNonValidUserId = generateMockedMongoDbId()
 
 export const testingNonPersistedUsername = 'non.persisted@mail.com'
 
