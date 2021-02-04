f9cca4a01069e5c557782f7dd07f379c5f3933d0
ddialar <ddialar@gmail.com>
2021-02-04 17:43:26 +0000

    test(checkPassword): Add a new testing case checking the method behavior when 'bcrypt' fails. All tests are in green.

--- src/domain/services/test/hash.servives/checkPassword.test.ts
+++ src/domain/services/test/hash.servives/checkPassword.test.ts
@@ -1,5 +1,8 @@
+import bcrypt from 'bcrypt'
+
 import { testingValidHashedPassword, testingValidPlainPassword, testingWrongPlainPassword } from '@testingFixtures'
 import { checkPassword } from '@domainServices'
+import { CheckingPasswordError } from '@errors'
 
 describe('[SERVICES] Hash - checkPassword', () => {
   const hashedPassword = testingValidHashedPassword
@@ -16,6 +19,22 @@ describe('[SERVICES] Hash - checkPassword', () => {
     const plainPassword = testingWrongPlainPassword
 
     expect((await checkPassword(plainPassword, hashedPassword))).toBeFalsy()
+
+    done()
+  })
+
+  it('must throw an INTERNAL_SERVER_ERROR (500) when the bcrypt lib fails', async (done) => {
+    jest.mock('bcrypt')
+
+    const plainPassword = testingValidPlainPassword
+    const errorMessage = 'Testing Error'
+    const expectedError = new CheckingPasswordError(`Error checking password. ${errorMessage}`)
+
+    bcrypt.compare = jest.fn().mockImplementationOnce(() => { throw expectedError })
+
+    await expect(checkPassword(plainPassword, hashedPassword)).rejects.toThrowError(expectedError)
+
+    jest.mock('bcrypt').resetAllMocks()
 
     done()
   })
