c47144ceabff01e7181d4d885ed2df52b4627187
ddialar <ddialar@gmail.com>
2021-02-04 17:24:56 +0000

    test(post.mappers): Refactor several testing suites in order to remove not needed as assertions. All tests are in green.

--- src/infrastructure/mappers/test/post.mappers/mapPostCommentFromDtoToDomainModel.test.ts
+++ src/infrastructure/mappers/test/post.mappers/mapPostCommentFromDtoToDomainModel.test.ts
@@ -2,12 +2,11 @@ import { PostCommentDomainModel } from '@domainModels'
 import { testingCommentedPersistedDtoPosts } from '@testingFixtures'
 
 import { mapPostCommentFromDtoToDomainModel } from '@infrastructure/mappers'
-import { PostCommentDto } from '@infrastructure/dtos'
 
 describe('[MAPPERS] Post mapper - mapPostCommentFromDtoToDomainModel', () => {
   it('maps successfully from Domain to DTO', () => {
     const [selectedPost] = testingCommentedPersistedDtoPosts
-    const [orifginalComment] = selectedPost.comments as PostCommentDto[]
+    const [orifginalComment] = selectedPost.comments
     const { _id: commentId, owner, ...otherCommentFields } = orifginalComment
     const { _id: commentOwnerId, userId, createdAt, updatedAt, ...otherCommentOwnerFields } = owner
 
