9ba0161a88ec797c162a5e6cfe0d5e265e1f1db8
ddialar <ddialar@gmail.com>
2021-02-05 22:17:51 +0000

    chore(express.dto): Include the 'loginData' firld in the 'RequestDto' interface.

--- src/infrastructure/server/serverDtos/express.dto.ts
+++ src/infrastructure/server/serverDtos/express.dto.ts
@@ -1,6 +1,8 @@
 import { Request } from 'express'
 import { UserDomainModel } from '@domainModels'
+import { LoginInputParams } from '@infrastructure/types'
 
 export interface RequestDto extends Request {
     user?: UserDomainModel | null
+    loginData?: LoginInputParams
 }
