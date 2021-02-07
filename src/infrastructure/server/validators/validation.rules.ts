83634421826deeb9a42e75032ce99232b2198103
ddialar <ddialar@gmail.com>
2021-02-07 18:30:41 +0000

    feat(server.validators): Create required and optional rules for 'name', 'surname' and 'avatar' fields.

--- src/infrastructure/server/validators/validation.rules.ts
+++ src/infrastructure/server/validators/validation.rules.ts
@@ -11,8 +11,11 @@ export const url = Joi.string().uri({
 
 export const username = email
 export const password = Joi.string().pattern(/^[a-zA-Z0-9]{4,}$/).required()
-export const name = Joi.string().min(2).required()
-export const surname = name
-export const avatar = url.required()
+export const optionalName = Joi.string().min(2)
+export const optionalSurname = optionalName
+export const optionalAvatar = url
+export const requiredName = Joi.string().min(2).required()
+export const requiredSurname = requiredName
+export const requiredAvatar = url.required()
 
 export const token = Joi.string().pattern(/^[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9-_]+$/).required()
