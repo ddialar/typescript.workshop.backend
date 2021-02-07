40412affe62d8b785cc69e435e64947f58eee0c9
ddialar <ddialar@gmail.com>
2021-02-07 18:35:15 +0000

    feat(serverDtos): Update the 'RequestDto' definition including the 'newProfileData' field.

--- src/infrastructure/server/serverDtos/express.dto.ts
+++ src/infrastructure/server/serverDtos/express.dto.ts
@@ -1,10 +1,11 @@
 import { Request } from 'express'
 import { UserDomainModel } from '@domainModels'
 import { LoginInputParams } from '@infrastructure/types'
-import { NewUserInputDto } from '@infrastructure/dtos'
+import { NewUserInputDto, NewUserProfileDto } from '@infrastructure/dtos'
 
 export interface RequestDto extends Request {
     user?: UserDomainModel | null
     loginData?: LoginInputParams
     signinData?: NewUserInputDto
+    newProfileData?: NewUserProfileDto
 }
