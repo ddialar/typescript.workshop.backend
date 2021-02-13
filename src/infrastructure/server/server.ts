b50befc0dba064ab50e5c6a91643452cf1de8c12
ddialar <ddialar@gmail.com>
2021-02-13 22:41:25 +0000

    refactor(server): Update the logger definition.

--- src/infrastructure/server/server.ts
+++ src/infrastructure/server/server.ts
@@ -10,8 +10,7 @@ import { authenticationRoutes, userRoutes, postRoutes } from './routes'
 
 import { handleHttpError } from './middlewares'
 
-import { createLogger } from '@common'
-const logger = createLogger('server')
+import { serverLogger } from '@logger'
 
 const app = express()
 const port = parseInt(process.env.SERVER_PORT ?? '3000', 10)
@@ -31,10 +30,10 @@ app.use(handleHttpError)
 
 const server = createServer(app)
 
-const runServer = () => server.listen(port, () => logger.info(`Server running in http://localhost:${port}`))
+const runServer = () => server.listen(port, () => serverLogger('info', `Server running in http://localhost:${port}`))
 
 const stopServer = () => {
-  logger.info('Clossing server...')
+  serverLogger('info', 'Clossing server...')
   server.close()
 }
 
