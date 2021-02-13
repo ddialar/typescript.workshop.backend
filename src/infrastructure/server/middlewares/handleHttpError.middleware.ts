cadf22ba2e74b3477199d5b9508c62ee4abe0b17
ddialar <ddialar@gmail.com>
2021-02-13 22:41:53 +0000

    refactor(middlewares): Update the logger definition.

--- src/infrastructure/server/middlewares/handleHttpError.middleware.ts
+++ src/infrastructure/server/middlewares/handleHttpError.middleware.ts
@@ -1,13 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
-import { createLogger } from '@common'
+import { serverLogger } from '@logger'
 import { ApiError } from '@errors'
-
-const logger = createLogger('server')
 
 export const handleHttpError = (error: ApiError, req: Request, res: Response, next: NextFunction): void => {
   const { status, message, description } = error
 
-  logger.error(`${message}${description ? ' - ' + description : ''}`)
+  serverLogger('error', `${message}${description ? ' - ' + description : ''}`)
 
   res.status(status).send({ error: true, message })
 }
