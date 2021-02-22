ab727ac9b83a610f580e4833ec594efc7c79052a
ddialar <ddialar@gmail.com>
2021-02-22 16:51:19 +0000

    test(infrastructure.mappers): Edit the 'mapPostFromDtoToDomainModel' testing suite in order to remove useless testing case which checked if the mapper returned null.

--- src/infrastructure/mappers/test/post.mappers/mapPostFromDtoToDomainModel.test.ts
+++ src/infrastructure/mappers/test/post.mappers/mapPostFromDtoToDomainModel.test.ts
@@ -3,10 +3,6 @@ import { testingLikedAndCommentedPersistedDtoPosts, testingLikedAndCommentedPers
 import { mapPostFromDtoToDomainModel } from '@infrastructure/mappers'
 
 describe('[MAPPERS] Post mapper - mapPostFromDtoToDomainModel', () => {
-  it('returns NULL when we don\'t provide data to be mapped', () => {
-    expect(mapPostFromDtoToDomainModel(null)).toBeNull()
-  })
-
   it('maps successfully from DTO to Domain', () => {
     const [originalPost] = testingLikedAndCommentedPersistedDtoPosts
     const [expectedPost] = testingLikedAndCommentedPersistedDomainModelPosts
