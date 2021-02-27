5314aa89d0cc0722511f0df142111a1c982b9aa3
ddialar <ddialar@gmail.com>
2021-02-27 14:31:20 +0000

    feat(domain.models): Edit the 'RegisteredUserDomainModel' type in order to include the user's avatar.

--- src/domain/models/user.domain.models.ts
+++ src/domain/models/user.domain.models.ts
@@ -15,7 +15,7 @@ export interface UserDomainModel {
 }
 
 export type NewUserDomainModel = Pick<UserDomainModel, 'username' | 'password' | 'email' | 'name' | 'surname' | 'avatar'>
-export type RegisteredUserDomainModel = Pick<UserDomainModel, 'username'> & { fullName: string }
+export type RegisteredUserDomainModel = Pick<UserDomainModel, 'username' | 'avatar'> & { fullName: string }
 export type UpdateUserPayloadDomainModel = Omit<Partial<UserDomainModel>, 'id' | 'username' | 'email' | 'createdAt' | 'updatedAt'>
 export type UserProfileDomainModel = Pick<UserDomainModel, 'username' | 'email' | 'name' | 'surname' | 'avatar'>
 export type NewUserProfileDomainModel = Partial<Pick<UserDomainModel, 'name' | 'surname' | 'avatar'>>
