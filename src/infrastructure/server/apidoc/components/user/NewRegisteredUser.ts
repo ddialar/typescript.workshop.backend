a44a71c9f3b0eb39db56dbcbee40ff227a37262c
ddialar <ddialar@gmail.com>
2021-02-27 14:37:11 +0000

    chore(server.apidoc): Update the 'NewRegisteredUser' component in order to include the user's 'avatar' field.

--- src/infrastructure/server/apidoc/components/user/NewRegisteredUser.ts
+++ src/infrastructure/server/apidoc/components/user/NewRegisteredUser.ts
@@ -11,6 +11,11 @@ export const newRegisteredUserComponent = {
       fullName: {
         type: 'string',
         example: 'Mike Wazowski'
+      },
+      avatar: {
+        type: 'string',
+        description: 'URL to the avatar location.',
+        example: 'https://cdn.icon-icons.com/icons2/1736/PNG/128/4043268-alien-avatar-space-ufo_113272.png'
       }
     }
   }
